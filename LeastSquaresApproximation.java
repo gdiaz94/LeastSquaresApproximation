0304fe48670ce9acf7c2cab14fa84bb807618d74
Gabriel Diaz <diazga01@pfw.edu>
2021-05-02 23:04:40 -0400

    First commit

---
+++ LeastSquaresApproximation.java
@@ -0,0 +1,14 @@
+// Programmer: 	Gabe Diaz
+// Date:		05/02/21
+// Course:		Numerical Analysis CS384
+// Purpose:		Implement an algorithm for calculating the least squares approximation (Linear and Polynomial)
+
+public class LeastSquaresApproximation 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+}
